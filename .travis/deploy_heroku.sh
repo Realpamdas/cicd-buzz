91d0b27e1ec262ee3df135b37cd7deb213a6db1a
Pam D <pdas@box.com>
2020-01-06 14:30:13 -0800

    Incorporate new changes for Heroku

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku container:login
+#wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
+# https://github.com/heroku/heroku-container-registry
+# https://devcenter.heroku.com/articles/heroku-cli
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
+heroku plugins:install @heroku-cli/plugin-container-registry
+docker login --username=_ --password=$HEROKU_API_KEY registry.heroku.com
+#heroku container:login
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app $HEROKU_APP_NAME

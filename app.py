f0c4ccb6e27b2b2f0bff198f86e50858dba5d72f
Pam D <pdas@box.com>
2020-01-08 15:58:59 -0800

    Updated Path for background file

--- app.py
+++ app.py
@@ -1,15 +1,20 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from buzz import generator
 
 app = Flask(__name__)
 
 
 @app.route("/")
+#def generate_buzz():
+#    page = '<html><body background="{{ url(NaturePatterns08.jpg) }}"><h1>'
+#    page += generator.generate_buzz()
+#    page += '</h1></body></html>'
+#    return page
+
 def generate_buzz():
-    page = '<html><body background="{{ url(NaturePatterns08.jpg) }}"><h1>'
+    page = render_template('home.html')
     page += generator.generate_buzz()
-    page += '</h1></body></html>'
     return page
 
 

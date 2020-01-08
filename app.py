42fa4c719c6dee58b9e241a43440189cfd5132a7
Pam D <pdas@box.com>
2020-01-08 15:28:22 -0800

    Updated Path for background file

--- app.py
+++ app.py
@@ -4,12 +4,14 @@ from buzz import generator
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def generate_buzz():
-    page = '<html><body background="{{ url_for('static', filename='NaturePatterns08.jpg') }}"><h1>'
+    page = '<html><body background="{{ url(NaturePatterns08.jpg) }}"><h1>'
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
